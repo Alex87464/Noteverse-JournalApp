129087e825f73d51de8a4569f954fa256efc5938
Alex87464 <alexoliva87464@gmail.com>
2023-07-31 22:14:54 -0300

    1st Restart

--- src/games/pages/SearchPage.jsx
+++
@@ -1,9 +0,0 @@
-import { SearchBar } from "../components/SearchBar";
-
-export const SearchPage = () => {
-  return (
-    <>
-      <SearchBar />
-    </>
-  );
-};
--- src/games/pages/index.js
+++
@@ -1,4 +0,0 @@
-export * from './GamesPage';
-export * from './GamesForm';
-export * from './SearchPage';
-export * from './DetailsPage';
