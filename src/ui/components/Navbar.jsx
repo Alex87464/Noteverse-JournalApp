a577aaae08567034c84972b5efcf67edd1c019a5
Alex87464 <alexoliva87464@gmail.com>
2023-08-01 01:54:15 -0300

    Fixed some bugs in the navbar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -12,8 +12,13 @@ import CategoryIcon from "@mui/icons-material/Category";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import StoreIcon from "@mui/icons-material/Store";
 import ReceiptIcon from "@mui/icons-material/Receipt";
-import BarChartIcon from "@mui/icons-material/BarChart"; // Importa el icono de análisis de ventas
-import SearchIcon from "@mui/icons-material/Search"; // Importa el icono de búsqueda de producto
+import BarChartIcon from "@mui/icons-material/BarChart";
+import SearchIcon from "@mui/icons-material/Search";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn"; // Icono de Ventas
+import AssignmentIcon from "@mui/icons-material/Assignment"; // Icono de Reportes
+import DashboardIcon from "@mui/icons-material/Dashboard"; // Icono de Dashboard
+import PeopleIcon from "@mui/icons-material/People"; // Icono de Clientes
+import ExitToAppIcon from "@mui/icons-material/ExitToApp"; // Icono de Cerrar Sesión
 
 const useStyles = makeStyles((theme) => ({
   drawer: {
@@ -25,9 +30,40 @@ const useStyles = makeStyles((theme) => ({
 const Navbar = () => {
   const classes = useStyles();
 
+
+
+  const handleLogout = () => {
+    // Aquí puedes realizar las acciones necesarias para cerrar la sesión del usuario
+    console.log("Se ha cerrado la sesión");
+  };
+
   return (
     <Drawer variant="permanent" className={classes.drawer}>
       <div className={classes.toolbar} />
+      <List>
+        <ListItemButton>
+          <ListItemIcon>
+            <DashboardIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItemButton>
+
+        <ListItemButton>
+          <ListItemIcon>
+            <AssignmentIcon />
+          </ListItemIcon>
+          <ListItemText primary="Reportes" />
+        </ListItemButton>
+
+        <ListItemButton>
+          <ListItemIcon>
+            <PeopleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Clientes" />
+        </ListItemButton>
+      </List>
+
+      <Divider />
       <List>
         <ListItemButton>
           <ListItemIcon>
@@ -35,25 +71,36 @@ const Navbar = () => {
           </ListItemIcon>
           <ListItemText primary="Productos" />
         </ListItemButton>
+
         <ListItemButton>
           <ListItemIcon>
             <CategoryIcon />
           </ListItemIcon>
           <ListItemText primary="Categorías" />
         </ListItemButton>
+
         <ListItemButton>
           <ListItemIcon>
             <BarChartIcon />
           </ListItemIcon>
           <ListItemText primary="Análisis de Ventas" />
         </ListItemButton>
+
         <ListItemButton>
           <ListItemIcon>
             <SearchIcon />
           </ListItemIcon>
           <ListItemText primary="Buscar Producto" />
         </ListItemButton>
+
+        <ListItemButton>
+          <ListItemIcon>
+            <MonetizationOnIcon />
+          </ListItemIcon>
+          <ListItemText primary="Ventas" />
+        </ListItemButton>
       </List>
+
       <Divider />
       <List>
         <ListItemButton>
@@ -73,6 +120,16 @@ const Navbar = () => {
             <ReceiptIcon />
           </ListItemIcon>
           <ListItemText primary="Órdenes de Compra" />
+        </ListItemButton>
+      </List>
+
+      <Divider />
+      <List>
+        <ListItemButton onClick={handleLogout}> {/*handleLogout*/}
+          <ListItemIcon>
+            <ExitToAppIcon />
+          </ListItemIcon>
+          <ListItemText primary="Cerrar Sesión" />
         </ListItemButton>
       </List>
     </Drawer>
