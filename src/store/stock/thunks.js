eaeb22a57c4683f60046e3e986a528a69350bac1
Alex87464 <alexoliva87464@gmail.com>
2023-08-04 05:21:28 -0300

    Created the thunks for the clothes

---
+++ src/store/stock/thunks.js
@@ -0,0 +1,20 @@
+export const startNewClothe = () => {
+
+    return async( dispatch ) => {
+        // uid del usuario
+
+        const newClothe = {
+            title: '',
+            body: '',
+            date: new Date().getTime(),
+        }
+
+
+    }
+
+    //! dispatch 
+    // dispatch( newClothe )
+    // dispatch( activarClothe )
+
+
+}
